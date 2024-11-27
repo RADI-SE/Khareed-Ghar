2d8011b5bac4d58a9dc7831063c2d001c7ee8517
RADI-SE <radi1y@hotmail.com>
2024-11-27 14:39:01 -0800

    - Integrated AuthRoutes with RouterProvider and ensured proper routing for authentication pages. - Updated uthRoutes to include proper paths for signin, signup, erify-email, etc.

--- FrontEnd/src/pages/AuthRoutes.jsx
+++ FrontEnd/src/pages/AuthRoutes.jsx
@@ -3,8 +3,8 @@ import SignIn from "../components/Auth/login/SignIn";
 import SignUp from "../components/Auth/register/SignUp";
 import ForgotPassword from "../components/Auth/ForgotPassword";
 import EmailSentSuccess from "../components/Auth/EmailSentSuccess";
-import { Outlet } from 'react-router-dom'; 
-
+import { Outlet } from 'react-router-dom';
+ 
 const AuthRoutes = () => {
   
   return (
