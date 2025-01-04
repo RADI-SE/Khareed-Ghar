7c570eb25620aabc5e1e4a371a4f845aaf8f1e19
RADI-SE <radi1y@hotmail.com>
2025-01-03 22:48:25 -0800

    Integrated validation middleware into category and subcategory routes

--- BackEnd/route/admin/manage.category.js
+++ BackEnd/route/admin/manage.category.js
@@ -11,6 +11,18 @@ import {
   getAllCategoryProducts,
   searchCategoryById,
 } from "../../controller/admin/category.controller.js";
+import  {
+  addCategoryValidation,
+  addSubCategoryValidation,
+  editCategoryValidation,
+  editSubCategoryValidation,
+  deleteCategoryValidation,
+  deleteSubCategoryValidation,
+  getSubCategoriesValidation,
+  getAllCategoryProductsValidation,
+  searchCategoryByIdValidation,
+  validate,
+} from "../../middleware/validators/category.validator.js"
 import { AuthorizeRoles } from "../../middleware/AuthorizeRoles.js";
 import { verifyTokenForRole } from "../../middleware/verifyTokenForRole.js";
 
@@ -21,24 +33,24 @@ const verifyAdmin = [verifyTokenForRole, AuthorizeRoles("admin")];
 const verifyAdminSeller = [verifyTokenForRole, AuthorizeRoles("admin", "seller")];
 
 // Routes for category and subcategory management
-router.post("/add-category", verifyAdmin, addCategory);
+router.post("/add-category",addCategoryValidation,validate, verifyAdmin, addCategory);
 
-router.post("/add-subcategory",  verifyAdmin, addSubCategory);
+router.post("/add-subcategory",addSubCategoryValidation,validate,  verifyAdmin, addSubCategory);
 
-router.get("/view-categories", getCategories);
+router.get("/view-categories",getAllCategoryProductsValidation,validate, getCategories);
 
-router.get("/get-category-byId/:id", searchCategoryById);
+router.get("/get-category-byId/:id",searchCategoryByIdValidation,validate, searchCategoryById);
 
 router.get("/get-products-by-category/:id", getAllCategoryProducts);
 
-router.get("/view-subcategories/:parentCategory", getSubCategories);
+router.get("/view-subcategories/:parentCategory",getSubCategoriesValidation,validate, getSubCategories);
 
-router.put("/edit-category/:id", verifyAdmin, editCategories);
+router.put("/edit-category/:id",editCategoryValidation,validate, verifyAdmin, editCategories);
 
-router.put("/edit-subcategory/:parentCategory", verifyAdmin, editSubCategories);
+router.put("/edit-subcategory/:parentCategory",editSubCategoryValidation,validate, verifyAdmin, editSubCategories);
 
-router.delete("/delete-category", verifyAdmin, deleteCategory);
+router.delete("/delete-category",deleteCategoryValidation,validate, verifyAdmin, deleteCategory);
 
-router.delete("/delete-subcategory", deleteSubategory);
+router.delete("/delete-subcategory",deleteSubCategoryValidation,validate, deleteSubategory);
 
 export default router;
