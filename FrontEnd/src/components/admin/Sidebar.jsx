bd5db2b5c54fdb02178d0c488be6c434c000897c
Muhammad-Sohail-Khan-Niazi <mskniazi01@gmail.cmm>
2025-04-11 21:16:57 +0500

    Section update

--- FrontEnd/src/components/admin/Sidebar.jsx
+++ FrontEnd/src/components/admin/Sidebar.jsx
@@ -76,7 +76,7 @@ const Sidebar = () => {
               </li>
               <li>
                 <button className=" mt-10 text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2" onClick={handleLogout}>
-                  LOGOUT
+                  LOGOUT 
                 </button>
               </li>
             </ul>
