25241f7f98449c61d7adede85c8870eeaeb7d51f
Syed-Suleman-Shah <28579@students.riphah.edu.pk>
2025-04-13 02:08:29 +0500

    Adding responsiveness to change/edit address modal

--- FrontEnd/src/components/Common/Modal.jsx
+++ FrontEnd/src/components/Common/Modal.jsx
@@ -1,9 +1,7 @@
-
-
 const Modal = ({ isModelOpen, setIsModelOpen, children }) => {
   if (!isModelOpen) return null;
   return (
-    <div className="fixed inset-0 bg-gray-800 bg-opacity-75 flex items-center justify-center z-50">
+    <div className="fixed inset-0 bg-gray-800 bg-opacity-75 flex items-start justify-center z-50 pt-16">
       <div className="bg-white w-50 rounded-lg shadow-lg h-auto">
         <button 
           className="absolute top-4 right-4 text-gray-300 text-3xl"
